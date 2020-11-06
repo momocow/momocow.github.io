01fc6920e40c352e43d2f21375d8d242c47aec0f
taocheng <taocheng@qnap.com>
2020-11-06 18:13:11 +0800

    :sparkles: Add main page entry.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useTranslation } from 'gatsby-plugin-react-i18next'
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Portfolio() {
+  const { t } = useTranslation()
+  return <div>{t('metadata:name')}</div>
 }
